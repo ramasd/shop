94647be888bfd7e2ea8e99efd009ddc8b6d44095
ramasd <ramunas.danupas@gmail.com>
2020-03-24 17:15:53 +0200

    Eloquent Relationships: Products Migrations/Models/Views

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,8 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::all();
+        $products = Product::with('category')->get();
 
-        return view('index', compact('categories'));
+        return view('index', compact('categories', 'products'));
     }
 }
